c7cca45b9df43926a8d6ff2121a047c6b2308f6f
lowlyherotech@gmail.com <[lowlyherotech@gmail.com]>
2019-04-02 16:09:00 -0400

    finished everything but stretch problems in closure.js, one problem left in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,30 +26,67 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+
+getLength(items, function (getLength) {
+    console.log(getLength);
+});
+
+
+//End of first problem
+
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    return cb(arr[3]);
 }
+
+last(items, function (last) {
+    console.log(last);
+});
+
+//End of second problem
+
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x, y);
 }
+
+const add = function (x, y) {
+    return x + y;
+}
+
+console.log(sumNums(2, 2, add));
+
+//End of third problem
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x, y);
+}
+const multiply = function (x, y) {
+    return x * y;
 }
 
+console.log(multiplyNums(2, 8, multiply));
+
+//End of forth problem
+
+
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
 }
+
+console.log(contains());
+
+
+//End of fifth problem
+
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+    // removeDuplicates removes all duplicate values from the given array.
+    // Pass the duplicate free array to the callback function.
+    // Do not mutate the original array.
 }
